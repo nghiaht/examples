f4d649743d5c01e40e6e36f9f0a2e216448f76c8
Nghia Huynh Trong <nghia.ht@hotmail.com>
2021-07-21 15:10:05 +0700

    Use interval to init fb to fix lost FB chat plugin when changing page

--- with-fb/src/fb/Facebook1.js
+++ with-fb/src/fb/Facebook1.js
@@ -1,27 +1,41 @@
 import { useEffect } from "react";
+import { clearInterval } from "timers";
+
+function loadScript() {
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+}
+
+function init() {
+  var chatbox = document.getElementById("fb-customer-chat");
+  chatbox.setAttribute("page_id", "1011747472244365");
+  chatbox.setAttribute("attribution", "biz_inbox");
+
+  // window.fbAsyncInit = function () {
+  FB.init({
+    xfbml: true,
+    version: "v11.0",
+  });
+  // };
+}
 
 export function Facebook1() {
   useEffect(() => {
-    var chatbox = document.getElementById("fb-customer-chat");
-    chatbox.setAttribute("page_id", "1011747472244365");
-    chatbox.setAttribute("attribution", "biz_inbox");
-
-    window.fbAsyncInit = function () {
-      FB.init({
-        xfbml: true,
-        version: "v11.0",
-      });
-    };
-
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
+    loadScript();
+    var interval = window.setInterval(() => {
+      if (window.FB) {
+        window.clearInterval(interval);
+        interval = null;
+        init();
+      }
+    }, 500);
   }, []);
   return (
     <div>
