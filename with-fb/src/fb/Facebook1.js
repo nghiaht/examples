7bde76bf73248a8203d18fc06a12a8190c85fb44
Nghia Huynh Trong <nghia.ht@hotmail.com>
2021-07-21 15:53:04 +0700

    Test again

--- with-fb/src/fb/Facebook1.js
+++ with-fb/src/fb/Facebook1.js
@@ -1,62 +1,16 @@
 import { useEffect } from "react";
-
-function init() {
-  var chatbox = document.getElementById("fb-customer-chat");
-  chatbox.setAttribute("page_id", "1011747472244365");
-  chatbox.setAttribute("attribution", "biz_inbox");
-
-  window.fbAsyncInit = function () {
-    FB.init({
-      xfbml: true,
-      version: "v11.0",
-    });
-  };
-
-  (function (d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
-}
-
-function reload() {  
-  console.log("reload")
-  window.FB.XFBML.parse();
-}
+import { init, cleanup } from "./fb";
 
 export function Facebook1() {
   useEffect(() => {
-    console.log("Facebook1")
+    console.log("Facebook1");
     init();
 
-    // var interval = window.setInterval(() => {
-    //   console.log("Facebook1 interval")
-    //   if (window.FB && window.FB.XFBML) {
-    //     window.clearInterval(interval);
-    //     interval = null;
-    //     reload();
-    //   }
-    // }, 500);
-
-    return function cleanup() {
-      console.log("cleanup");
-      (function (d, id) {
-        var target =  d.getElementById(id);
-        if (target) {
-          target.parentNode.removeChild(target);
-          
-        }        
-      })(document, "facebook-jssdk");
-
-      if (window.FB) {
-        delete window.FB;
-      }
-    }
+    return () => {
+      cleanup();
+    };
   }, []);
+
   return (
     <div>
       <div id="fb-root"></div>
--- with-fb/src/fb/Facebook2.js
+++ with-fb/src/fb/Facebook2.js
@@ -1,43 +1,14 @@
 import { Component } from "react";
+import { init, cleanup } from "./fb";
 
 export class Facebook2 extends Component {
   componentDidMount() {
-    var chatbox = document.getElementById("fb-customer-chat");
-    chatbox.setAttribute("page_id", "1011747472244365");
-    chatbox.setAttribute("attribution", "biz_inbox");
-
-    window.fbAsyncInit = function () {
-      FB.init({
-        xfbml: true,
-        version: "v11.0",
-      });
-    };
-
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-
-    var interval = window.setInterval(() => {
-      console.log("interval")
-      if (window.FB && window.FB.XFBML) {
-        window.clearInterval(interval);
-        interval = null;
-        this.reload();
-      }
-    }, 500);
+    init();
   }
 
-  reload() {  
-    console.log("reload")
-    window.FB.XFBML.parse();
+  componentWillUnmount() {
+    cleanup();
   }
-  
 
   render() {
     return (
